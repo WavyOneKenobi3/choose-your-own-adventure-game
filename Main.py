2152412b7a79167749388dc92894b5a16feec43e
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-14 15:02:07 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+#choose your own path
+print("Welcome to Tresure island. \n Your mission is to find the treasure")
+
+
+
+
+
+
+while True:
+    ltoR_user = input("You're at a cross road. Where do you want to go? type Left or Right").lower() #one option is game over
+    if ltoR_user == "left":
+        print("Game over")
+    elif ltoR_user =="right":
+        wait_swim = input("You come to lake. There is an island in the middle of the lake. Type wait to wait for the boat. Type swim to swim across. ").lower() #1 option is game over 
+        if wait_swim == "swim":
+            print("Game over")
+        elif wait_swim == "wait":
+            doors = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? ").lower()  #1 door is game over
+            if doors == "red":
+                print("Game Over")
+            elif doors == "blue":
+                print("Game Over")
+            elif doors == "yellow":
+                print("You win!")
+                break
+            
+            
